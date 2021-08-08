d04e94444766f06d39edd9aaa4de47c58c5f717d
Tiabeany <tiaesanto@gmail.com>
2021-08-08 03:09:50 +0200

    Adding GetByRentalId on IBookingRepository

--- VacationRental.Core/Repositories/IBookingRepository.cs
+++ VacationRental.Core/Repositories/IBookingRepository.cs
@@ -1,8 +1,10 @@
-﻿using VacationRental.Core.Models;
+﻿using System.Collections.Generic;
+using VacationRental.Core.Models;
 
 namespace VacationRental.Core.Repositories
 {
     public interface IBookingRepository : IRepository<Booking>
     {
+        IEnumerable<Booking> GetByRentalId(int rentalId);
     }
 }
