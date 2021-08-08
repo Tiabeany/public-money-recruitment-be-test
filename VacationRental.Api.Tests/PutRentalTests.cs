64266f49ef5bfbb569d7a3bb3efd34b34df1afb1
Tiabeany <tiaesanto@gmail.com>
2021-08-08 18:08:33 +0200

    Creating PutRentalTests

---
+++ VacationRental.Api.Tests/PutRentalTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using VacationRental.Api.Models;
+using Xunit;
+
+namespace VacationRental.Api.Tests
+{
+    [Collection("Integration")]
+    public class PutRentalTests
+    {
+        private readonly HttpClient _client;
+
+        public PutRentalTests(IntegrationFixture fixture)
+        {
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        public async void GivenCompleteRequest_WhenPutRental_ThenAGetReturnsTheUpdatedRental()
+        {
+            var request = new RentalBindingModel
+            {
+                Units = 25,
+                PreparationTimeInDays = 1
+            };
+
+            ResourceIdViewModel postResult;
+            using (var postResponse = await _client.PostAsJsonAsync($"/api/v1/rentals", request))
+            {
+                Assert.True(postResponse.IsSuccessStatusCode);
+                postResult = await postResponse.Content.ReadAsAsync<ResourceIdViewModel>();
+            }
+
+            using (var getResponse = await _client.GetAsync($"/api/v1/rentals/{postResult.Id}"))
+            {
+                Assert.True(getResponse.IsSuccessStatusCode);
+
+                var getResult = await getResponse.Content.ReadAsAsync<RentalViewModel>();
+                Assert.Equal(request.Units, getResult.Units);
+            }
+
+            ResourceIdViewModel putResult;
+            using (var putResponse = await _client.PutAsJsonAsync($"/api/v1/rentals/{postResult.Id}", request))
+            {
+                Assert.True(putResponse.IsSuccessStatusCode);
+                putResult = await putResponse.Content.ReadAsAsync<ResourceIdViewModel>();
+            }
+
+            using (var getResponse = await _client.GetAsync($"/api/v1/rentals/{putResult.Id}"))
+            {
+                Assert.True(getResponse.IsSuccessStatusCode);
+
+                var getResult = await getResponse.Content.ReadAsAsync<RentalViewModel>();
+                Assert.Equal(request.Units, getResult.Units);
+            }
+
+        }
+    }
+}
