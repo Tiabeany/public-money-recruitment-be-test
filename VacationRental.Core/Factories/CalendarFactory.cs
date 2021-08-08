91310f9891732f1d838dbcac1fbd5bdea06fd11a
Tiabeany <tiaesanto@gmail.com>
2021-08-08 04:22:35 +0200

    Add CalendarFactory

---
+++ VacationRental.Core/Factories/CalendarFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using VacationRental.Core.Models;
+
+namespace VacationRental.Core.Factories
+{
+    public static class CalendarFactory
+    {
+        public static Calendar CreateCalendar(List<Booking> allRentalBookings, int nights, int rentalId, DateTime start)
+        {
+            var calendar = new Calendar(new List<CalendarDate>(), rentalId);
+
+            for (var i = 0; i < nights; i++)
+            {
+                var date = start.Date.AddDays(i);
+                var calendarDate = new CalendarDate(date, new List<CalendarBooking>(), new List<CalendarPreparationTime>());
+                foreach (var booking in allRentalBookings)
+                {
+                    if (booking.Start <= date.Date && booking.Start.AddDays(booking.Nights) > date.Date)
+                    {
+                        calendarDate.AddCalendarBooking(new CalendarBooking(booking.Id, booking.Unit));
+                    }
+                }
+                calendar.AddCalendarDate(calendarDate);
+            }
+
+            return calendar;
+        }
+
+        public static Calendar CreateCalendar(object p, int nights, int rentalId, DateTime start)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
